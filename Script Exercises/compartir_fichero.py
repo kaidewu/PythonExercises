ed8b13ccd7ecf680a8cb4670252be385f0c6b351
kaidewu <kaidewu@protonmail.com>
2022-02-08 17:40:31 +0100

    Update compartir_fichero.py

--- Script Exercises/compartir_fichero.py
+++ Script Exercises/compartir_fichero.py
@@ -3,24 +3,22 @@
 #Compartirlos
 #grupo usuarios tenga permiso de modificación
 import csv
-from dataclasses import replace
 import os
-from re import I
-import shutil
 import sys
 
-#try:
-with open('directorios.csv', 'r') as carpetas:
-    diretorio = csv.reader(carpetas)
-    for d in diretorio:
-        dirX = ''.join(d)
-        lista = dirX.split('\\')
-        nomFolder = lista[-1]
-        if os.path.exists(nomFolder):
-            os.system(f'net share {nomFolder}=K:\{d} /GRANT:everyone,CHANGE')
-        else:
-            os.system(f'mkdir {d}')
-            os.system(f'net share {nomFolder}=K:\{d} /GRANT:everyone,CHANGE')
-#except:
-#    print('ERROR!')
-#    sys.exit(1)
+try:
+    with open('directorios.csv', 'r') as carpetas:
+        diretorio = csv.reader(carpetas)
+        for d in diretorio:
+            dirX = ''.join(d)
+            lista = dirX.split('\\')
+            nomFolder = lista[-1]
+            print(dirX)
+            if os.path.exists(nomFolder):
+                os.system(f'net share {nomFolder}={dirX} /GRANT:Usuario,CHANGE')
+            else:
+                os.system(f'mkdir {dirX}')
+                os.system(f'net share {nomFolder}={dirX} /GRANT:Usuario,CHANGE')
+except:
+    print('ERROR!')
+    sys.exit(1)
