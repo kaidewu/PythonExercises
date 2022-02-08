8dfffa21e085bfc7f80164db31d72c45ee87b8c0
kaidewu <kaidewu@protonmail.com>
2022-02-08 17:50:45 +0100

    Update compartir_fichero.py

--- Script Exercises/compartir_fichero.py
+++ Script Exercises/compartir_fichero.py
@@ -10,12 +10,15 @@ try:
     with open('directorios.csv', 'r') as carpetas:
         diretorio = csv.reader(carpetas)
         for d in diretorio:
+            # Tenemos que convertir el directorio de lista a character
             dirX = ''.join(d)
+            # Para poder sacar el nombre de la carpeta que queremos compartir, tenemos que quitar el "\"
             lista = dirX.split('\\')
             nomFolder = lista[-1]
-            print(dirX)
+            # Primero miramos si la carpeta existe o no. Si existe que comparta las carpetas
             if os.path.exists(nomFolder):
                 os.system(f'net share {nomFolder}={dirX} /GRANT:Usuarios,CHANGE')
+            # Si no existe que cree las carpetas y los comparta
             else:
                 os.system(f'mkdir {dirX}')
                 os.system(f'net share {nomFolder}={dirX} /GRANT:Usuarios,CHANGE')
