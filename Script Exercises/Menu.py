e0cda017ac61b8c358e041f63a2312dd7c2d8bef
kaidewu <kaidewu@protonmail.com>
2022-02-08 18:27:25 +0100

    Upload new exercises: Menu.py

---
+++ Script Exercises/Menu.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import socket
+
+def cambiarIP():
+    try:
+        address = input('Dime la IP que quieres: ')
+        submask = input('Dime la mascara que tendra: ')
+        gateway = input('Dime la puerta de enlace: ')
+        interface = input('Dime que interfaz quieres cambiar:')
+    except:
+        print('No has puesto bien los parametros!')
+        sys.exit(1)
+    os.system(f'netsh interface ipv4 set address name="{interface}" static {address} {submask} {gateway}')
+    print(f'Se ha cambiado correctamente!. Tu nueva IP es {address} con la mascara {submask} y la puerta de enlace {gateway}, en el interfaz {interface}')
+
+def cambiarNombre():
+    try:
+        name = input('Dime el nuevo nombre para el equipo: ')
+    except:
+        print('No has puesto bien los parametros!')
+        sys.exit(1)
+    os.system(f'netdom renamecomputer %{socket.gethostname()}% /Newname "{name}"')
+while accion != 7:
+    accion = int(input('1 (Cambiar tu IP del ordenador)'))
