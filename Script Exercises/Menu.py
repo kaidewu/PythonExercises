de417bb58825e69e82bdb33ac83422b80ab0e456
Kaide <kaidewu@protonmail.com>
2022-02-11 11:42:29 +0100

    Update python files

--- Script Exercises/Menu.py
+++ Script Exercises/Menu.py
@@ -1,6 +1,4 @@
-from nntplib import GroupInfo
 import os
-import sys
 import socket
 import csv
 
@@ -10,16 +8,29 @@ hostnameActual = socket.gethostname()
 def main():
     while True:
         print('''
-                                *------------------------------------------------------------------------------*
-                 |                                                                              |
-                 |                                                                              |
-                 |                                                                              |
-                 |                                 Bienvenido                                   |
-                 |                                                                              |
-                 |                                                                              |
-                 |                                                            Hecho por Kaide Wu|
-                 *------------------------------------------------------------------------------*''')
-        accion = input('1.- Cambiar tu IP del ordenador\n2.- Cambiar el nombre de la máquina\n3.- Promocionar\n4.- Crear unidades organizativas\n5.- Crear grupos\n6.- Crear usuarios\n7.- Salir\n¿Cuál quieres elegir?: ')
+*------------------------------------------------------------------------------*
+|                                                                              |
+|                                                                              |
+|                                                                              |
+|                                 Bienvenido                                   |
+|                                                                              |
+|                                                                              |
+|                                                            Hecho por Kaide Wu|
+*------------------------------------------------------------------------------*
+                 ''')
+        try:
+            #accion = input('1.- Cambiar tu IP del ordenador\n2.- Cambiar el nombre de la máquina\n3.- Promocionar\n4.- Crear unidades organizativas\n5.- Crear grupos\n6.- Crear usuarios\n7.- Salir\n¿Cuál quieres elegir?: ')
+            accion = input('''
+1.- Cambiar tu IP del ordenador
+2.- Cambiar el nombre de la máquina
+3.- Promocionar
+4.- Crear unidades organizativas
+5.- Crear grupos
+6.- Crear usuarios
+7.- Salir
+¿Cuál quieres elegir?: ''')
+        except:
+            print('Ha ocurrido un Error!!')
         if accion == '1':
             cambiarIP()
         elif accion == '2':
@@ -44,7 +55,7 @@ def cambiarIP():
         interface = input('Dime que interfaz quieres cambiar:')
     except:
         print('No has puesto bien los parametros!')
-        sys.exit(1)
+
     os.system(f'netsh interface ipv4 set address name="{interface}" static {address} {submask} {gateway}')
     print(f'Se ha cambiado correctamente!. Tu nueva IP es {address} con la mascara {submask} y la puerta de enlace {gateway}, en el interfaz {interface}')
 
@@ -53,7 +64,7 @@ def cambiarNombre():
         name = input('Dime el nuevo nombre para el equipo: ')
     except:
         print('No has puesto bien los parametros!')
-        sys.exit(1)
+
     os.system(f'netdom renamecomputer %{hostnameActual}% /Newname "{name}"')
 
 def promocionar():
@@ -62,7 +73,7 @@ def promocionar():
         netbiosName = input('Dime el nombre de la Netbios: ')
     except:
         print('Ha habido un error!!')
-        sys.exit(1)
+
 
     os.system(f'dcpromo /InstallDNS:Yes /ReplicaOrNewDomain:Domain /NewDomain:Forest /NewDomainDNSName:{domainDNSName} /ForestLevel:3 /DomainNetbiosName:{netbiosName} /DomainLevel:3 /ConfirmGc:Yes /CreateDNSDelegation:No /DatabasePath:\"C:\\Windows\\NTDS\" /LogPath:\"C:\\Windows\\NTDS\" /SYSVOLPath:\"C:\\Windows\\SYSVOL\"')
     os.system("shutdown -r -t 0")
@@ -75,51 +86,57 @@ def crearUO():
         print('Has puesto los parametros mal!!')
     domainName = domainName.split('.')
     if len(domainName) == 2:
-
-        os.system(f'dsadd ou ou={ouName},dc={domainName[0]},dc={domainName[1]}')
+        if os.path.exists(ouName):
+            with open(ouName, 'r') as csvfile:
+                files = csv.reader(csvfile)
+                for f in files:
+                    os.system(f'dsadd ou ou={f[0]},dc={domainName[0]},dc={domainName[1]}')
+        else:
+            print('No se ha encontrado el archivo csv!!!')
     else:
         print('Hay un error al poner el nombre del dominio!!!')
 
 def crearGrupos():
-    contador = 0
     try:
         groupfile = input('Dime la ruta del archivo csv: ')
-        if not os.path.exists(groupfile):
-            print('La ruta del archivo no es correacta!!')
+        domainName = input('Dime el nombre de tu dominio (ej. midominio.com): ')
     except:
         print('Ha habido un error en la ruta del archivo csv!!')
     try:
-        with open("grupos.csv", "r") as grupo:
-            lista = csv.reader(grupo)
-            for cada_linea in lista:
-                if contador == 1:
-                    grupo = cada_linea[0]
-                    uo = cada_linea[1]
-                    os.system(f"dsadd group \"cn={grupo}, cn={uo}, dc=dominio14, dc=local\" -secgrp yes -scope L")
-                contador = 1
+        domainName = domainName.split('.')
+        if len(domainName) == 2:
+            if not os.path.exists(groupfile):
+                print('La ruta del archivo no es correacta!!')
+            else:
+                with open(groupfile, "r") as grupo:
+                    lista = csv.reader(grupo)
+                    for cada_linea in lista:
+                            grupo = cada_linea[0]
+                            uo = cada_linea[1]
+                            os.system(f"dsadd group \"cn={grupo}, cn={uo}, dc={domainName[0]}, dc={domainName[1]}\" -secgrp yes -scope L")
     except:
         print('Ha habido un error')
 
 def crearUsuarios():
-    contador = 0
     try:
         userfile = input('Dime la ruta del archivo csv: ')
-        if not os.path.exists(userfile):
-            print('La ruta del archivo no es correacta!!')
+        domainName = input('Dime el nombre de tu dominio (ej. midominio.com): ')
     except:
         print('Ha habido un error en la ruta del archivo csv!!')
     try:
-        with open("usuarios.csv", "r") as usuarios:
-            lista = csv.reader(usuarios)
-            for cada_linea in lista:
-                if contador == 1:
-                    usuario = cada_linea[0]
-                    uo = cada_linea[1]
-                    passwd = cada_linea[2]
-                    os.system(f"dsadd user \"cn={usuario}, cn={uo}, dc=dominio14, dc=local\" -pwd {passwd} -disabled no")
-                contador = 1
+        domainName = domainName.split('.')
+        if len(domainName) == 2:
+            if not os.path.exists(userfile):
+                print('La ruta del archivo no es correacta!!')
+            else:
+                with open(userfile, "r") as grupo:
+                    lista = csv.reader(grupo)
+                    for cada_linea in lista:
+                            user = cada_linea[0]
+                            uo = cada_linea[1]
+                            password = cada_linea[2]
+                            os.system(f"dsadd user \"cn={user}, cn={uo}, dc={domainName[0]}, dc={domainName[1]}\" -pwd {password} -disabled no")
     except:
-        print("Ha habido un Error!")
-        sys.exit(-1)
+        print('Ha habido un error')
 
 main()
--- Script Exercises/prueba.py
+++ Script Exercises/prueba.py
@@ -1,12 +1,22 @@
+import csv
 import os
-from xml import dom
 try:
-    ouName = input('Dime el nombre de la unidad organizativa: ')
+    userfile = input('Dime la ruta del archivo csv: ')
     domainName = input('Dime el nombre de tu dominio (ej. midominio.com): ')
 except:
-    print('Has puesto los parametros mal!!')
-domainName = domainName.split('.')
-if len(domainName) == 2:
-    os.system(f'dsadd ou ou={ouName},dc={domainName[0]},dc={domainName[1]}')
-else:
-    print('Hay un error al poner el nombre del dominio!!!')
+    print('Ha habido un error en la ruta del archivo csv!!')
+try:
+    domainName = domainName.split('.')
+    if len(domainName) == 2:
+        if not os.path.exists(userfile):
+            print('La ruta del archivo no es correacta!!')
+        else:
+            with open(userfile, "r") as grupo:
+                lista = csv.reader(grupo)
+                for cada_linea in lista:
+                        user = cada_linea[0]
+                        uo = cada_linea[1]
+                        password = cada_linea[2]
+                        print(f"dsadd user \"cn={user}, cn={uo}, dc={domainName[0]}, dc={domainName[1]}\" -pwd {password} -disabled no")
+except:
+    print('Ha habido un error')
