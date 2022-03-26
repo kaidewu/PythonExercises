4a6204c3a6c6d836f9d5ab7f9b51ed636a78d9af
kaidewu <kaidewu@protonmail.com>
2022-03-26 19:05:34 +0100

    Updates in Wordle

--- Learning Python Exercises/Wordle/main.py
+++ Learning Python Exercises/Wordle/main.py
@@ -1,3 +1,20 @@
-import resquests
+from random_word import RandomWords
 
-word_site = ""
+def word_of_day():
+    while True:
+        word = RandomWords().get_random_word(minLength = 5, maxLength=5).upper()
+        if word != None or '-' not in word:
+            break
+    return word
+
+def main():
+    while True:
+        user_word = input('Enter a 5-letter Word: ').upper()
+        if len(user_word) == 5 or user_word != '':
+            pass
+    list_user_word = []
+    for letter_user_word in user_word:
+        list_user_word.append(letter_user_word)
+
+
+main()
