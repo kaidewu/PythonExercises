9890e8cc8eee564a02d863ef35029e755ff2f20e
kaidewu <kaidewu@protonmail.com>
2022-03-26 19:20:52 +0100

    Update Wordle Game

--- Learning Python Exercises/Wordle/main.py
+++ Learning Python Exercises/Wordle/main.py
@@ -1,20 +1,26 @@
 from random_word import RandomWords
+from prettytable import PrettyTable
 
 def word_of_day():
     while True:
-        word = RandomWords().get_random_word(minLength = 5, maxLength=5).upper()
+        word = RandomWords().get_random_word(minLength = 5, maxLength=5)
         if word != None or '-' not in word:
             break
-    return word
+    return word.upper()
+
+def letters_words(word):
+    list_letters = []
+    for letter_word in word:
+        list_letters.append(letter_word)
+    return list_letters
 
 def main():
     while True:
         user_word = input('Enter a 5-letter Word: ').upper()
         if len(user_word) == 5 or user_word != '':
-            pass
-    list_user_word = []
-    for letter_user_word in user_word:
-        list_user_word.append(letter_user_word)
+            break
+    list_user_word = letters_words(user_word)
+    list_main_word = letters_words(word_of_day())
 
 
 main()
